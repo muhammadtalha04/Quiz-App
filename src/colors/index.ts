20db749904c359908aed3057ade10041e409ffcb
Muhammad Talha <muhammadtalha04@gmail.com>
2021-04-28 13:42:04 +0500

    Changed the background gradient color of body tag

--- src/colors/index.ts
+++ src/colors/index.ts
@@ -1,6 +1,6 @@
 export const gradient_light = "#209AF1";
 export const gradient_dark = "#6D20D5";
-export const bg_gradient_light = "#1659FF";
+export const bg_gradient_light = "#5479FF";
 export const bg_gradient_dark = "#1C48E9";
 export const secondary = "#FFFFFF";
 export const black = "#404040";
